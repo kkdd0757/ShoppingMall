1140cbf1204dcde4f6673ef2dc3cc1c36eca4c0a
kkdd0757 <kkdd0757@hanmail.net>
2021-06-07 21:32:21 +0900

    1. 결제 총합 보여주는 기능 추가 2. cart.css 추가

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,6 +1,34 @@
+const display = document.getElementById('display');
+
 $(document).ready(function () {
     cart.init();
+    let totalPrice = getTotalPrice();
+    display.innerText = String(totalPrice);
+    // $('.quantity').keydown(function () {
+    //     totalPrice = getTotalPrice();
+    //     display.innerText = String(totalPrice);
+    // });
 })
+
+function getTotalPrice() {
+    let length = $(".quantity").length;
+    let totalPrice = 0;
+
+    for(let idx = 0; idx < length; idx++) {
+        let price = $(`#price_${idx}`).val();
+        let quantity = $(`#quantity_${idx}`).val();
+
+        totalPrice += price * quantity;
+    }
+
+    return totalPrice;
+}
+
+function setTotalPrice() {
+    let totalPrice = getTotalPrice();
+    display.innerText = String(totalPrice);
+}
+
 
 let cart = {
     init: function () {
@@ -28,8 +56,8 @@ let cart = {
             let header = $("meta[name='_csrf_header']").attr("content");
 
             $.ajax({
-                type:"POST",
-                url:"/payment", //보내는 주소 설정
+                type: "POST",
+                url: "/payment", //보내는 주소 설정
                 data: JSON.stringify({params: params}),
                 dataType: "json", //받아올 데이터(생략가능) html, json등 사용 가능
                 contentType: 'application/json; charset=utf-8;',
@@ -37,7 +65,7 @@ let cart = {
                     xhr.setRequestHeader(header, token);
                 },
 
-                success: (res)=>{
+                success: (res) => {
                     console.log(res);
                     if (res.code !== 200) {
                         alert("결제에 실패했습니다");
@@ -46,7 +74,9 @@ let cart = {
                     alert("결제에 성공했습니다.");
                     location.replace("/");
                 },
-                error: (log)=>{alert("실패"+log)}
+                error: (log) => {
+                    alert("실패" + log)
+                }
             });
         })
     },
